2c70bd847228c4cd8f313de231d7db75af282bd3
UROOBAYASEEN <uroobanawal74@gmail.com>
2025-01-07 13:14:57 -0800

    this is cooment

--- app/blogs/[blog]/page.tsx
+++ app/blogs/[blog]/page.tsx
@@ -150,7 +150,7 @@ console.log(blogsdata)
        <p className='mt-5 md:text-3xl text-light  tracking-tighter '>{blogsdata.detailnote}</p>
        <p className='mt-5 md:text-4xl text-light tracking-tighter text-red-700'>Comments</p>
        {comments.map((val,ind)=><div>
-        <div className='flex justify-start gap-6 'key={ind}>
+        <div className='flex justify-start gap-6 ' key={ind}>
           <p className='text-2xl font-bold text-green-700 mt-6'>{val.username}:</p>
           <p className='text-2xl font-bold text-red-700 mt-6'>{val.comment}</p>
         </div>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <div className='flex justify-between px-5 pt-4 fixed top-0 left-0 right-0'>
         <div className='text-2xl font-bold'><span className='text-4xl text-red-800 font-bold'>M</span>y <span  className='text-4xl text-red-800 font-bold'>B</span>logs</div>
         <div className='flex justify-between gap-5'>
-            {blogcategories.map((val)=><div>
+            {blogcategories.map((val,ind)=><div key={ind}>
                <Link href={val=="home"?`/`:`/${val}`}> <p className='uppercase text-[6px] md:text-2xl lg:text-1xl tracking-tighter font-bold'>{val}</p></Link>
             </div>)}
         </div>
